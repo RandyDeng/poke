f654d9c4755313451de7f819f491fe1373d978b9
Randy Deng <ryangdeng@gmail.com>
2019-07-10 23:00:02 -0400

    Added aws amplify and basic button for login

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import SignInScreen from './SignInScreen';
+import AuthLoadingScreen from './AuthLoading'
+// Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
+// goes here.
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+const AppStack = createStackNavigator({ Home: MainTabNavigator });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
