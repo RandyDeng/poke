01fb448d4a59b4095ee0804de132ece3447ea7c7
Randy Deng <ryangdeng@gmail.com>
2019-07-16 01:26:22 -0400

    Added UI for login page

--- navigation/SignInScreen.js
+++ navigation/SignInScreen.js
@@ -1,28 +1,105 @@
 import React from 'react';
-import {
-  AsyncStorage,
-  Button,
-  View,
-} from 'react-native';
-
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 class SignInScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Please sign in',
-  };
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    authCode: '',
+    user: {}
+  }
 
-  render() {
-    return (
-      <View>
-        <Button title="Sign in!" onPress={this._signInAsync} />
-      </View>
-    );
+  async signUp() {
+    const {
+      username,
+      password,
+      email
+    } = this.state
+    await Auth.signUp({
+      username,
+      password,
+      attributes: {
+        email
+      }
+    })
+    console.log('sign up successful!')
+  }
+
+  async confirmSignUp() {
+    const {
+      username,
+      authCode
+    } = this.state
+    await Auth.configSignignUp(username, authCode)
+    console.log('confirm sign up successful!')
+  }
+
+  async signIn() {
+    const {
+      username,
+      password
+    } = this.state
+    const user = await Auth.signIn(username, password)
+    this.setState({
+      user
+    })
+    console.log('sign in successful!')
+  }
+
+  async confirmSignIn() {
+    const {
+      user,
+      authCode
+    } = this.state
+    await Auth.configSignignIn(user, authCode)
+    console.log('user now successfully signed in to the app!!')
   }
 
   _signInAsync = async () => {
     await AsyncStorage.setItem('userToken', 'abc');
     this.props.navigation.navigate('App');
   };
+
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: 100
+      }}>
+        <View style={{
+          alignItems: 'center'
+        }}>
+          <Text style={{fontSize: 30}}>Poke</Text>
+          <TextInput
+            placeholder="  Username"
+            style={{ height: 40, width: 250, borderColor: '#333', borderWidth: 1 }}
+          />
+          <TextInput
+            placeholder="  Password"
+            style={{ height: 40, width: 250, borderColor: '#333', borderWidth: 1 }}
+          />
+        </View>
+
+        <View style={{
+          flexDirection: 'row',
+          paddingTop: 15,
+          justifyContent: 'center',
+          alignContent: 'space-between'
+        }}>
+          <Button
+            title="Sign In"
+            style={{ borderColor: '#333', borderWidth: 1 }}
+            onPress={this.handleSend} />
+          <Button
+            title="Sign Up"
+            style={{ borderColor: '#333', borderWidth: 1 }}
+            onPress={this.handleSend} />
+        </View>
+      </View>
+    );
+  }
 }
 
 export default SignInScreen
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import {
+  AsyncStorage,
+  Button,
+  View,
+} from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'app.json',
   };
 
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  };
+
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View>
+        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+      </View>
+    );
   }
 }
